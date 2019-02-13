d4e5cc6773e2ad4f91a63296413d55618419bbb5
Luis Menina <liberforce@freeside.fr>
2019-02-13 10:57:17 +0100

    Add a script to cleanup a variety of files
    Also serves as an example of how to use vim as an automation engine.

---
+++ scripts/beautify.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+ERR_BAD_ARGS=1
+ERR_VIM_NOT_FOUND=2
+
+tmpfile=$(mktemp --tmpdir "beautify.XXX")
+
+usage()
+{
+    cat << EOF
+Usage: $(basename $0) FILE [FILES]
+
+Processes one (or more) files to remove blank lines and trailing spaces.
+EOF
+}
+
+beautify()
+{
+    cat << EOF > "$tmpfile"
+:set hidden
+:argdo g/^\s*$/d | %s/\s\+$//e
+:wall
+:quit
+EOF
+
+    vim -s "$tmpfile" -es "$@"
+}
+
+main()
+{
+    if [ $# -lt 1 ]; then
+        usage
+        exit $ERR_BAD_ARGS
+    fi
+
+    VIM=$(which vim)
+    if [ ! -f "$VIM" ]; then
+        exit $ERR_VIM_NOT_FOUND
+    fi
+
+    beautify "$@"
+}
+
+main "$@"
